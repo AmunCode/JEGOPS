ae5e5663c78193b7f035c21076cb279ef485b2fc
AmunCode <gjose055@fiu.edu>
2022-07-18 17:21:39 -0400

    Created base file and empty index page that will serve as the home page. Added an auth.py file and added routes to the file for login.

---
+++ website/auth.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return "<h1>Login Page Placeholder</h1>"
+
+@auth.route('/login')
+def login():
+    return "<h1>Login Page Placeholder</h1>"
+
+@auth.route('/login')
+def login():
+    return "<h1>Login Page Placeholder</h1>"
+
+
+
