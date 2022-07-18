4fdc0a27dcd896216f209fe428efdbddd7e8bf1e
AmunCode <gjose055@fiu.edu>
2022-07-18 16:25:23 -0400

    added Flask app creation to __init__.py and code to safely create an app from main.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,1 +1,10 @@
 # file to create and manage package
+from flask import Flask
+
+
+# function to create flask app instance
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET__KEY'] = 'dummyDevKey'
+
+    return app
