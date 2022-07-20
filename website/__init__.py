8a95f12867b989b70174b2322b02a2e42aaf6372
AmunCode <gjose055@fiu.edu>
2022-07-20 12:49:29 -0400

    Updated login form. Added DB information to init file.

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,18 @@
 # file to create and manage package
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 
 
 # function to create flask app instance
 def create_app():
     app = Flask(__name__)
     app.config['SECRET__KEY'] = 'dummyDevKey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
