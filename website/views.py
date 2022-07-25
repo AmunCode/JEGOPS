9430736cebfa4383e589f988060e15eff829f4dd
AmunCode <gjose055@fiu.edu>
2022-07-25 17:27:41 -0400

    fixing data bug in views.py.

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
 # file to manage views
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 from api_calls import get_device_info
+from phone import Phone
 
 views = Blueprint('views', __name__)
 
@@ -17,21 +18,24 @@ def home():
 def dashboard():
     if request.method == 'POST':
         imei = request.form.get('imei')
-        scratches = request.form.get('scratches')
-        dents = request.form.get('dents')
-        lcd_discoloration = request.form.get('lcd-discoloration')
-        components_missing = request.form.get('missing-components')
-        cracked = request.form.get('cracks')
-        markings = request.form.get('markings')
+        # scratches = request.form.get('scratches')
+        # dents = request.form.get('dents')
+        # lcd_discoloration = request.form.get('lcd-discoloration')
+        # components_missing = request.form.get('missing-components')
+        # cracked = request.form.get('cracks')
+        # markings = request.form.get('markings')
         data = get_device_info(imei)
-
+        # cosmetic_data = request.form.to_dict()
         # Tracer lines
         print(data['BatteryHealthPercentage'])
-        print(f"scratches: {scratches}, dents {dents}, lcd {lcd_discoloration}, comps {components_missing},"
-              f" cracked {cracked}, markings {markings}")
+        # print(f"scratches: {scratches}, dents {dents}, lcd {lcd_discoloration}, comps {components_missing},"
+        #       f" cracked {cracked}, markings {markings}")
+        # testing_data = cosmetic_data.update(data)
+        # print(f"cosmetic data: {cosmetic_data}")
+        # print(f"full data: {testing_data}")
 
         if data:
-            return render_template('cosmetics.html', user=current_user, data=data)
+            return redirect(url_for('views.cosmetics', user=current_user, data=data))
 
     return render_template('dashboard.html', user=current_user)
 
